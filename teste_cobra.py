a58eaff703dda8db3b95b34582e2482d6b82af04
arturamorimb <amorimartur01@gmail.com>
2025-09-30 12:44:25 -0300

    pontuacao_colisao
    Implementado um sistema de pontuação a cada colisão, aparecendo no canto superior esquerdo.

--- teste_cobra.py
+++ teste_cobra.py
@@ -1,41 +1,50 @@
 import pygame, cores, sys
+from random import randint
 from pygame.locals import *
 
 def main():
 
     pygame.init()
 
+    fonte = pygame.font.SysFont('gabriola', 40, False, False)
+
     largura = 640
     altura = 480
 
-    l1_rect = 40
-    l2_rect = 50
+    h_rect = 50
+    w_rect = 50
 
+    x = largura/2 - w_rect/2
+    y = altura/2 - h_rect/2
 
-    x = largura/2 - l1_rect/2
-    y = altura/2 - l2_rect/2
+    x_azul = randint(0, largura - w_rect)
+    y_azul = randint(0, altura - h_rect)
 
     tela = pygame.display.set_mode((largura, altura))
     pygame.display.set_caption('Teste')
+
+    pontos = 0
 
     clock = pygame.time.Clock()
 
     while True:
         clock.tick(100)
         tela.fill((0, 0, 0))
+        mensagem = f'Pontos: {pontos}'
+        texto_formatado = fonte.render(mensagem, True, cores.branco)
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
 
             if pygame.key.get_pressed()[K_w]:
-                y -= 20
+                y -= 10
             if pygame.key.get_pressed()[K_a]:
-                x -= 20
+                x -= 10
             if pygame.key.get_pressed()[K_s]:
-                y += 20
+                y += 10
             if pygame.key.get_pressed()[K_d]:
-                x += 20
+                x += 10
             
             if x > largura:
                 x = 0 
@@ -47,9 +56,17 @@ def main():
             if y < 0:
                 y = altura
 
-        pygame.draw.rect(tela, cores.vermelho, (x, y, l1_rect, l2_rect))
-        pygame.draw.rect(tela, cores.azul, (200, 300, l1_rect, l2_rect))
+        ret_vermelho = pygame.draw.rect(tela, cores.vermelho, (x, y, w_rect, h_rect))
+        ret_azul = pygame.draw.rect(tela, cores.azul, (x_azul, y_azul, w_rect, h_rect))
+
+        if ret_vermelho.colliderect(ret_azul):
+            x_azul = randint(0, largura - w_rect)
+            y_azul = randint(0, altura - h_rect)
+            pontos += 1
+
+        tela.blit(texto_formatado, (10, 10))
 
         pygame.display.update()  
 
-main()
+if __name__ == '__main__':
+    main()
